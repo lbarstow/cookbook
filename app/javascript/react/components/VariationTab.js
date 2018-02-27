f124a9791ca4cf8711dec401ac0ba4e385c44c94
lbarstow <lbarstow@macalester.edu>
2018-02-26 23:42:52 -0500

    add variation tab test

--- app/javascript/react/components/VariationTab.js
+++ app/javascript/react/components/VariationTab.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const VariationTab = (props) =>{
-  // need to put in  a better way to format my body
-
   return(
     <div id={props.id} onClick={props.onClick} className={ props.isSelected ? "tab selected" : "tab"}>
       {props.title}
--- spec/javascript/react/components/VariationTabTest.js
+++ spec/javascript/react/components/VariationTabTest.js
@@ -12,7 +12,6 @@ describe('VariationTab', () => {
         onClick={testFunc}
         isSelected={true}
         title="Banana Bread"
-
       />
     )
   })
@@ -22,34 +21,18 @@ describe('VariationTab', () => {
     expect(wrapper.find('div').text()).toBe("Banana Bread");
   });
 
-  it('should have five spans', () => {
-    expect(wrapper.find('span').length).toEqual(5)
+  it('the div should have an id of 2', () => {
+    expect(wrapper.props().id).toEqual(2);
   });
 
-  it('should have an a source span', () => {
-    expect(wrapper.find('span').at(0).text()).toBe("From: common knowledge");
+  it('should have an a selected class', () => {
+    const wrapperDiv = wrapper.find('div');
+    expect(wrapperDiv.hasClass("selected")).toEqual(true);
+  });
+  it('should have an div with the recipe title as text', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').text()).toBe("Banana Bread");
   });
 
-  it('should have an a make servings span', () => {
-    expect(wrapper.find('span').at(1).text()).toBe("Makes: one glass");
-  });
-
-  it('the last span contains author and date', () => {
-    expect(wrapper.find('span').last().text()).toBe(`Added on ${createDate} by Laura`);
-  });
-
-  it('should have two p tags', () => {
-    expect(wrapper.find('p').length).toEqual(2);
-  });
-
-  it('should have the description in the first paragraph', () => {
-    expect(wrapper.find('p').at(0).text()).toBe("Description: I'm a description");
-  });
-
-  it('should have the body in the last paragraph', () => {
-    const bodyParagraph = wrapper.find('p').at(1);
-    expect(bodyParagraph.find('span').at(0).text()).toBe("pour water into a glass ");
-    expect(bodyParagraph.find('span').at(1).text()).toBe(" Add Ice");
-  });
 
 })
